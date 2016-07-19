3f0e6783551d5f06797ff886114ade196dc058c0
Dex Hunter <dixingxu@gmail.com>
2016-07-19 21:59:11 +0800

    finish task3 in two line!

--- Assignment1/program3.R
+++ Assignment1/program3.R
@@ -1,3 +1,4 @@
+```
 ud <- function(df){
   #df[1:2] <- t( apply(df[1:2], 1, sort) )
   out <- df[duplicated(df[1:2]),] #hash function
@@ -5,5 +6,14 @@ ud <- function(df){
   write.table(out, file="D:/out.txt", sep=" ", row.names=FALSE, col.names = FALSE)
   #return (out)
 }
+```
+#new version below
+
+ud <- function(df){
+  out <- df[duplicated(df[1:2]),]
+  write.table(out[1:2], file="D:/out88.txt", sep=" ", row.names=FALSE, col.names = FALSE)
+}
+
+
 
 
