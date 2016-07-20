76aed4e32077d8dfebb146f1a6cafad321eba38c
Dex Hunter <dixingxu@gmail.com>
2016-07-20 10:27:41 +0800

    Correct some typos

--- Assignment1/program3.R
+++ Assignment1/program3.R
@@ -1,7 +1,7 @@
 ```
 ud <- function(df){
-  #df[1:2] <- t( apply(df[1:2], 1, sort) )
-  out <- df[duplicated(df[1:2]),] #hash function
+  df[1:2] <- t( apply(df[1:2], 1, sort) )
+  out <- duplicated(df[1:2],fromLast=TRUE) #hash function
   out[3] <- NULL #this line can be integrated into the above
   write.table(out, file="D:/out.txt", sep=" ", row.names=FALSE, col.names = FALSE)
   #return (out)
