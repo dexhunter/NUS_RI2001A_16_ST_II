173eda3be82db83e9ada46af5db4c2672a97aabc
Dex Hunter <dixingxu@gmail.com>
2016-07-20 10:30:05 +0800

    correct typos!

--- Assignment1/program3.R
+++ Assignment1/program3.R
@@ -1,21 +1,16 @@
 ```
+#sort first and find duplicated ones
 ud <- function(df){
   df[1:2] <- t( apply(df[1:2], 1, sort) )
-<<<<<<< HEAD
-  out <- duplicated(df[1:2],fromLast=TRUE) #hash function
-=======
-  out <- df[duplicated(df[1:2]),] #hash function
->>>>>>> origin/master
-  out[3] <- NULL #this line can be integrated into the above
-  write.table(out, file="D:/out.txt", sep=" ", row.names=FALSE, col.names = FALSE)
-  #return (out)
+  out <- duplicated(df[1:2],fromLast=TRUE)
+  write.table(out[1:2], file="D:/out.txt", sep=" ", row.names=FALSE, col.names = FALSE)
 }
 ```
 #new version below
 
 ud <- function(df){
   out <- df[duplicated(df[1:2]),]
-  write.table(out[1:2], file="D:/out88.txt", sep=" ", row.names=FALSE, col.names = FALSE)
+  write.table(out[1:2], file="D:/out.txt", sep=" ", row.names=FALSE, col.names = FALSE)
 }
 
 
